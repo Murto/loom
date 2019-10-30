4d48a7abd6e233f992b6a57ee4489fcd9dcebf2b
Murray Steele <contact@murto.dev>
2019-10-30 18:07:28 +0000

    Add tokenization

--- loom/loom.py
+++ loom/loom.py
@@ -1,8 +1,52 @@
 #!/usr/bin/env python3
 
 import argparse
+import enum
+import re
 import sys
 
+
+class TokenType(enum.Enum):
+    LET = 0
+    ASSIGN = 1
+    NEWLINE = 2
+    LEFT_BRACE = 3
+    RIGHT_BRACE = 4
+    ASTERISK = 5
+    IN = 6
+    COMMA = 7
+    SYMBOL = 8
+    CHARACTER = 9
+    STRING = 10
+
+class Token:
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+
+def tokenize(source):
+    token_map = {
+        re.compile('let') : TokenType.LET,
+        re.compile('=') : TokenType.ASSIGN,
+        re.compile('\n') : TokenType.NEWLINE,
+        re.compile('{') : TokenType.LEFT_BRACE,
+        re.compile('}') : TokenType.RIGHT_BRACE,
+        re.compile('\\*') : TokenType.ASTERISK,
+        re.compile('in') : TokenType.IN,
+        re.compile(',') : TokenType.COMMA,
+        re.compile('\\w+') : TokenType.SYMBOL,
+        re.compile('\'\\S\'') : TokenType.CHARACTER,
+        re.compile('"\\S*"') : TokenType.STRING
+    }
+    while len(source) > 0:
+        for pattern, type in token_map.items():
+            result = pattern.match(source)
+            if result:
+                value = result.group()
+                source = source[result.end:]
+                yield Token(type, value)
 
 def parse_arguments():
     description = 'Loom is a toy programming language by Murray Steele'
