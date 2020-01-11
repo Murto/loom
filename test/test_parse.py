176cb38341b3194d2383cdac171f2b879c72ab69
Murray Steele <contact@murto.dev>
2020-01-11 11:49:53 +0000

    Change expected AST defintion bit lists to strings

--- test/test_parse.py
+++ test/test_parse.py
@@ -18,19 +18,19 @@ class TestParse(unittest.TestCase):
         self.expect(EXPECTED, FILE_PATH)
 
     def test_language_definition(self):
-        EXPECTED = Program([LanguageDefinition(Symbol('test'), Set([String([0, 1, 0, 1]), String([])]))])
+        EXPECTED = Program([LanguageDefinition(Symbol('test'), Set([String('0101'), String('')]))])
         FILE_PATH = os.path.join(DATA_PATH, 'language_definition_parse.lm')
         self.expect(EXPECTED, FILE_PATH)
 
     def test_string_definition(self):
-        EXPECTED = Program([StringDefinition(Symbol('test'), String([0, 1, 0, 1]), Symbol('test'))])
+        EXPECTED = Program([StringDefinition(Symbol('test'), String('0101'), Symbol('test'))])
         FILE_PATH = os.path.join(DATA_PATH, 'string_definition_parse.lm')
         self.expect(EXPECTED, FILE_PATH)
 
     def test_program(self):
         EXPECTED = Program(                            \
-            [LanguageDefinition(Symbol('strings'), ProductExpression(Set([String([0]), String([1])]), Set([String([0]), String([1])]))), \
-            StringDefinition(Symbol('zero'), ConcatenateExpression(String([0]), String([0])), Symbol('strings'))])
+            [LanguageDefinition(Symbol('strings'), ProductExpression(Set([String('0'), String('1')]), Set([String('0'), String('1')]))), \
+            StringDefinition(Symbol('zero'), ConcatenateExpression(String('0'), String('0')), Symbol('strings'))])
         FILE_PATH = os.path.join(DATA_PATH, 'binary.lm')
         self.expect(EXPECTED, FILE_PATH)
 
